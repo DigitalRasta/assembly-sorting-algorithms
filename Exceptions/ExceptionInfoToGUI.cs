29c5e743705cb475c34b60faa02fedc8c5ee086f
DigitalRasta <jakubbujny@gmail.com>
2015-01-20 14:09:07 +0100

    Report added, not completed yet

--- Exceptions/ExceptionInfoToGUI.cs
+++ Exceptions/ExceptionInfoToGUI.cs
@@ -1,12 +1,5 @@
 ﻿using System;
 
-/*
- * Exception which provides user-readable informations about Exception in application
- * Author: Jakub'Digitalrasta'Bujny
- * Version: 0.0.0
- * Created: 22.10.2014
- * Changelog:
- */
 namespace sortingProject.Exceptions
 {
     public class ExceptionInfoToGUI : System.Exception
